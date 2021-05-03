aea7b3dfa401d8134d3252445c3168b5c0898961
Philippe Guyard <philippe.guyard@outlook.com>
2021-05-04 00:42:01 +0200

    hotfix

---
+++ tanks/examples/up_shooter.py
@@ -0,0 +1,15 @@
+from random import randint
+
+MOVE_UP = 0
+MOVE_RIGHT = 1
+MOVE_DOWN = 2
+MOVE_LEFT = 3
+SHOOT_UP = 4
+SHOOT_RIGHT = 5
+SHOOT_DOWN = 6
+SHOOT_LEFT = 7
+STAND_STILL = 8
+
+def play_turn(grid, player_pos):
+    return SHOOT_UP
+    
